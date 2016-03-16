7f205a2556e326af469fe16fa6f624f14da970ac
baghbidi <srbaghbidi@yahoo.com>
2016-03-16 09:53:58 -0700

    "status" report

--- src/com/iotracks/iofabric/status_reporter/StatusReporter.java
+++ src/com/iotracks/iofabric/status_reporter/StatusReporter.java
@@ -2,8 +2,6 @@ package com.iotracks.iofabric.status_reporter;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import com.iotracks.iofabric.field_agent.FieldAgentStatus;
@@ -11,11 +9,14 @@ import com.iotracks.iofabric.local_api.LocalApiStatus;
 import com.iotracks.iofabric.message_bus.MessageBusStatus;
 import com.iotracks.iofabric.process_manager.ProcessManagerStatus;
 import com.iotracks.iofabric.resource_consumption_manager.ResourceConsumptionManagerStatus;
+import com.iotracks.iofabric.supervisor.Supervisor;
 import com.iotracks.iofabric.supervisor.SupervisorStatus;
+import com.iotracks.iofabric.utils.Constants.ControllerStatus;
 import com.iotracks.iofabric.utils.logging.LoggingService;
 
 public final class StatusReporter {
 	
+	private static int SET_SYSTEM_TIME_FREQ_SECONDS = 60;
 	private static SupervisorStatus supervisorStatus = new SupervisorStatus();
 	private static ResourceConsumptionManagerStatus resourceConsumptionManagerStatus = new ResourceConsumptionManagerStatus();
 	private static FieldAgentStatus fieldAgentStatus = new FieldAgentStatus();
@@ -40,6 +41,8 @@ public final class StatusReporter {
 		final DateFormat df = new SimpleDateFormat("MMM dd yyyy hh:mm:ss.SSS");
 		
 		float diskUsage = resourceConsumptionManagerStatus.getDiskUsage();
+		String connectionStatus = fieldAgentStatus.getContollerStatus() == ControllerStatus.OK ? "ok" : 
+			(fieldAgentStatus.getContollerStatus() == ControllerStatus.BROKEN ? "broken" : "not provisioned"); 
 		
 		result.append("ioFabric daemon             : " + supervisorStatus.getDaemonStatus().name());
 		result.append("\nMemory Usage                : about " + String.format("%.2f", resourceConsumptionManagerStatus.getMemoryUsage()) + " MiB");
@@ -49,8 +52,8 @@ public final class StatusReporter {
 			result.append("\nDisk Usage                  : about " + String.format("%.2f", diskUsage) + " GiB");
 		result.append("\nCPU Usage                   : about " + String.format("%.2f", resourceConsumptionManagerStatus.getCpuUsage()) + "%");
 		result.append("\nRunning Elements            : " + processManagerStatus.getRunningElementsCount());
-		result.append("\nConnection to Controller    : [ok][broken][not provisioned]");				// TODO : get from field agent
-		result.append("\nMessages Processed          : about 1,583,323"); 							// TODO : get from message bus 
+		result.append("\nConnection to Controller    : " + connectionStatus);
+		result.append(String.format("\nMessages Processed          : about %,d", messageBusStatus.getProcessedMessages()));  
 		result.append("\nSystem Time                 : " + df.format(statusReporterStatus.getSystemTime()));
 		
 		return result.toString();
@@ -96,8 +99,7 @@ public final class StatusReporter {
 	}
 
 	public static void start() {
-		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-		scheduler.scheduleAtFixedRate(setStatusReporterSystemTime, 0, 60, TimeUnit.SECONDS);
+		Supervisor.scheduler.scheduleAtFixedRate(setStatusReporterSystemTime, 0, SET_SYSTEM_TIME_FREQ_SECONDS, TimeUnit.SECONDS);
 		LoggingService.logInfo(MODULE_NAME, "started");
 	}
 
