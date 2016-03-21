c632b11a88d067143b0d235d8aa528d437248ce7
ashitanagar <ashita.nagar15@gmail.com>
2016-03-21 14:58:21 -0700

    Local API

--- src/com/iotracks/iofabric/local_api/MessageSenderHandler.java
+++ src/com/iotracks/iofabric/local_api/MessageSenderHandler.java
@@ -1,11 +1,11 @@
 package com.iotracks.iofabric.local_api;
 
-
 import static io.netty.handler.codec.http.HttpMethod.*;
 import static io.netty.handler.codec.http.HttpResponseStatus.*;
 import static io.netty.handler.codec.http.HttpVersion.*;
 
 import java.io.StringReader;
+import java.util.concurrent.Callable;
 
 import javax.json.Json;
 import javax.json.JsonBuilderFactory;
@@ -13,66 +13,67 @@ import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 
-import org.bouncycastle.util.Arrays;
-
 import com.iotracks.iofabric.message_bus.Message;
 import com.iotracks.iofabric.message_bus.MessageBus;
 import com.iotracks.iofabric.utils.logging.LoggingService;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.util.CharsetUtil;
 
-public class MessageSenderHandler {
+public class MessageSenderHandler implements Callable<Object> {
 	private final String MODULE_NAME = "Local API";
 
-	public void handle(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception{
+	private final FullHttpRequest req;
+	private ByteBuf bytesData;
+
+	public MessageSenderHandler(FullHttpRequest req, ByteBuf bytesData) {
+		this.req = req;
+		this.bytesData = bytesData;
+	}
+
+	public Object handleMessageSenderRequest() throws Exception{
 
 		LoggingService.logInfo(MODULE_NAME,"In MessageSenderHandler : handle");		
 		HttpHeaders headers = req.headers();
 
 		if (req.getMethod() != POST) {
-			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.METHOD_NOT_ALLOWED));
-			return;
+			return new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.METHOD_NOT_ALLOWED);
 		}
 
 		if(!(headers.get(HttpHeaders.Names.CONTENT_TYPE).equals("application/json"))){
-			ByteBuf	errorMsgBytes = ctx.alloc().buffer();
 			String errorMsg = " Incorrect content/data format ";
-			errorMsgBytes.writeBytes(errorMsg.getBytes());
-			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.BAD_REQUEST, errorMsgBytes));
-			return;
+			bytesData.writeBytes(errorMsg.getBytes());
+			return new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.BAD_REQUEST, bytesData);
 		}
 
 		ByteBuf msgBytes = req.content();
 		String msgString = msgBytes.toString(io.netty.util.CharsetUtil.US_ASCII);
-		
+
 		LoggingService.logInfo(MODULE_NAME,"body :"+ msgString);
 		JsonReader reader = Json.createReader(new StringReader(msgString));
 		JsonObject jsonObject = reader.readObject();
 
 		if(validateMessage(jsonObject) != null){
 			LoggingService.logInfo(MODULE_NAME,"Validation Error...");
-			ByteBuf	errorMsgBytes = ctx.alloc().buffer();
 			String errorMsg = validateMessage(jsonObject);
-			errorMsgBytes.writeBytes(errorMsg.getBytes());
-			sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.BAD_REQUEST, errorMsgBytes));
-			return;
+			bytesData.writeBytes(errorMsg.getBytes());
+			return new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.BAD_REQUEST, bytesData);
 		}
-		
+
 		//Publish message on message bus
 		LoggingService.logInfo(MODULE_NAME,"Message validation successful.. ");
+
+		long lStartTime = System.currentTimeMillis();
 		MessageBus bus = MessageBus.getInstance();
 		Message message = new Message(jsonObject);
 		Message messageWithId = bus.publishMessage(message);
+		long lEndTime = System.currentTimeMillis();
+		long difference = lEndTime - lStartTime;
+		System.out.println("Message Bus Retrival elapsed milliseconds: " + difference);
 		
 		LoggingService.logInfo(MODULE_NAME,"id " + messageWithId.getId() + "timestamp" + messageWithId.getTimestamp());
 
@@ -83,13 +84,10 @@ public class MessageSenderHandler {
 		builder.add("id", messageWithId.getId());
 
 		String configData = builder.build().toString();
-		ByteBuf	bytesData = ctx.alloc().buffer();
 		bytesData.writeBytes(configData.getBytes());
 		FullHttpResponse res = new DefaultFullHttpResponse(HTTP_1_1, OK, bytesData);
 		HttpHeaders.setContentLength(res, bytesData.readableBytes());
-
-		sendHttpResponse( ctx, req, res); 
-		return;
+		return res;
 	}
 
 	private String validateMessage(JsonObject message) throws Exception{
@@ -148,20 +146,9 @@ public class MessageSenderHandler {
 		return null;
 	}
 
-
-	private static void sendHttpResponse(
-			ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse res) throws Exception{
-		if (res.getStatus().code() != 200) {
-			ByteBuf buf = Unpooled.copiedBuffer(res.getStatus().toString(), CharsetUtil.UTF_8);
-			res.content().writeBytes(buf);
-			buf.release();
-			HttpHeaders.setContentLength(res, res.content().readableBytes());
-		}
-
-		ChannelFuture f = ctx.channel().writeAndFlush(res);
-		if (!HttpHeaders.isKeepAlive(req) || res.getStatus().code() != 200) {
-			f.addListener(ChannelFutureListener.CLOSE);
-		}
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return handleMessageSenderRequest();
 	}
 }
-
